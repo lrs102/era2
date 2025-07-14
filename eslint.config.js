602814ad682d704def9505632af7929299ed1138
DJ <dj.xmx@proton.me>
2025-07-14 14:42:31 -0400

    Adding some eslint and prettier configs.

--- eslint.config.js
+++ eslint.config.js
@@ -2,22 +2,40 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tseslint, * as reactDom from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs['recommended-latest'],
+            reactRefresh.configs.vite,
+            reactDom.configs.recommended,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parser: tseslint.parser,
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        plugins: {
+            prettier: prettier,
+        },
+        rules: {
+            // Put rules you want to override here
+            "react-dom/no-dangerously-set-innerhtml": "warn",
+            "prettier/prettier": "error",
+            // Turn off rules that might conflict with Prettier
+            "max-len": "off",
+            "no-mixed-spaces-and-tabs": "off",
+        },
     },
-  },
 ])
